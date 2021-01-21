9144c52d64a218e7bf893056c56e539cc0757c23
Daniel Grace <daniel.grace44@hotmail.com>
2021-01-21 19:47:31 +0000

    added route to search sheet page

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Dashboard from "@/components/Dashboard";
 import ViewNightSheet from "@/components/ViewNightSheet";
 import NewNightSheet from "@/components/NewNightSheet";
 import ListNightSheets from "@/components/ListNightSheets";
+import Search from "@/components/Search";
 
 Vue.use(Router);
 
@@ -13,27 +14,32 @@ export default new Router({
     {
       path: "/",
       name: "dashboard",
-      component: Dashboard
+      component: Dashboard,
     },
     {
       path: "/new",
       name: "new-night-sheet",
-      component: NewNightSheet
+      component: NewNightSheet,
     },
     {
       path: "/list",
       name: "list-night-sheets",
-      component: ListNightSheets
+      component: ListNightSheets,
     },
     {
       path: "/edit/:sheet_id",
       name: "edit-night-sheet",
-      component: EditNightSheet
+      component: EditNightSheet,
+    },
+    {
+      path: "/search",
+      name: "search",
+      component: Search,
     },
     {
       path: "/:sheet_id",
       name: "view-night-sheet",
-      component: ViewNightSheet
-    }
-  ]
+      component: ViewNightSheet,
+    },
+  ],
 });
