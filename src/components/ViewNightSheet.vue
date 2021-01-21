6c559910b9a9e9d215b908959a77e6d713de16b5
Daniel Grace <daniel.grace44@hotmail.com>
2021-01-21 19:48:12 +0000

    added delete option to sheet and added alert prompt

--- src/components/ViewNightSheet.vue
+++ src/components/ViewNightSheet.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container>
         <v-card
-        class="mx-auto"
+        class="mx-auto mt-10"
         max-width="1000"
         tile
         >
@@ -13,8 +13,7 @@
             </v-card-subtitle>
             <v-card-text>
                 <strong>Name:</strong> {{sheet.name}}
-            </v-card-text>
-            <v-card-text>
+                <br>
                 <strong>Notes:</strong> {{sheet.notes}}
             </v-card-text>
             <v-card-actions>
@@ -22,6 +21,8 @@
                 outlined
                 rounded
                 text
+                class="white--text" 
+                color="blue darken-3"
                 :to="`edit/${sheet.id}`"
                 >
                     <v-icon left>
@@ -29,8 +30,56 @@
                     </v-icon>
                     Edit
                 </v-btn>
+                <v-btn
+                outlined
+                rounded
+                text
+                class="white--text" 
+                color="red darken-3"
+                @click.stop="dialog = true"
+                >
+                    <v-icon left>
+                        mdi-file-remove-outline
+                    </v-icon>
+                    Delete
+                </v-btn>
             </v-card-actions>
         </v-card>
+        <v-dialog
+        v-model="dialog"
+        max-width="290"
+        >
+            <v-card>
+                <v-card-title class="headline">
+                Delete this Sheet?
+                </v-card-title>
+
+                <v-card-text>
+                If you delete this sheet it will be no longer accessible from the database are you sure you would like to delete it?
+                </v-card-text>
+
+                <v-card-actions>
+                <v-spacer></v-spacer>
+
+                <v-btn
+                    color="blue darken-3"
+                    text
+                    @click="dialog = false"
+                >
+                    Cancel
+                </v-btn>
+
+                <v-btn
+                    color="red darken-3"
+                    text
+                    @click="deleteSheet"
+                    :to="`/list`"
+                >
+                    Delete
+                </v-btn>
+                </v-card-actions>
+            </v-card>
+    </v-dialog>
     </v-container>
 </template>
 
@@ -46,7 +95,8 @@ export default {
                 date: '',
                 day: '',
                 notes: ''
-            }
+            },
+            dialog:false
         }
     },
     created () {
@@ -58,6 +108,18 @@ export default {
                 this.sheet.day = doc.data().Day
                 this.sheet.notes = doc.data().Notes
         })
+    },
+    methods:{
+        deleteSheet () {
+            if(confirm("Are you sure you want to delte this sheet?")){
+                const sheetID = this.$route.params.sheet_id
+
+                db.collection('night-sheets').doc(sheetID).get().then(doc => {                         
+                    // doc.ref.delete()
+                    console.log(doc.data())
+                })
+            }
+        }
     }
 } 
 </script>
