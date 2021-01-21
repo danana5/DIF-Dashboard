9902a7d53689eedb2072d8736b5e24b62b40d45a
Daniel Grace <daniel.grace44@hotmail.com>
2021-01-21 22:32:44 +0000

    Implemented edit night sheet component

--- src/components/EditNightSheet.vue
+++ src/components/EditNightSheet.vue
@@ -1,17 +1,166 @@
 <template>
-    <div id="EditNightSheet">
-        <h3>EditNightSheet
-    
-        </h3>
-    </div>
+    <v-container>
+        <v-card
+        class="mx-auto mt-10"
+        max-width="1000"
+        tile
+        >
+            <v-card-title>
+                Edit Night Sheet
+            </v-card-title>
+            <v-container>
+                <v-form
+                ref="form"
+                v-model="valid"
+                lazy-validation
+                >
+                    <v-text-field
+                    v-model="sheetName"
+                    :rules="nameRules"
+                    label="Name"
+                    placeholder="Name"
+                    required
+                    clearable
+                    >
+                    </v-text-field>
+                    <v-text-field
+                    v-model="date"
+                    :rules="dateRules"
+                    label="Date"
+                    placeholder="DD/MM/YYYY"
+                    required
+                    clearable
+                    >
+                    </v-text-field>
+                    <v-text-field
+                    v-model="day"
+                    :rules="dayRules"
+                    label="Day"
+                    placeholder="Day"
+                    required
+                    clearable
+                    >
+                    </v-text-field>
+                    <v-textarea
+                    v-model="notes"
+                    label="Notes"
+                    auto-grow
+                    placeholder="Notes"
+                    clearable
+                    >
+                    </v-textarea>
+                    <v-spacer></v-spacer>
+                    <v-divider></v-divider>
+                    <v-spacer></v-spacer>
+                    <v-row>
+                        <v-col>
+                            <v-text-field
+                            v-model="cash"
+                            :rules="cashRules"
+                            label="Cash Recieved"
+                            required
+                            prepend-icon="€"
+                            clearable
+                            >
+                            </v-text-field>
+                        </v-col>
+                        <v-col>
+                            <v-text-field
+                            v-model="card"
+                            :rules="cardRules"
+                            label="Card Recieved"
+                            required
+                            prepend-icon="€"
+                            clearable
+                            >
+                            </v-text-field>
+                        </v-col>
+                    </v-row>
+                    <v-btn
+                    class="white--text mr-4" 
+                    color="red darken-3"
+                    outlined
+                    rounded
+                    text
+                    :to="`/`"
+                    >
+                        Cancel
+                    </v-btn>
+                    <v-btn
+                    class="white--text" 
+                    color="blue darken-3"
+                    outlined
+                    :disabled="!valid"
+                    rounded
+                    text
+                    @click="updateSheet"
+                    >
+                        Update Sheet
+                    </v-btn>
+                </v-form>
+            </v-container>
+        </v-card>
+    </v-container>
 </template>
 
 <script>
+import db from './firebaseInit'
 export default {
     name: 'EditNightSheet',
     data () {
         return {
-            
+            valid: true,
+            date: null,
+            sheetName: null,
+            day: null,
+            notes: null,
+            card: null,
+            cash: null,
+            total: null,
+            nameRules: [
+                v => !!v || 'Name is required',
+            ],
+            dateRules: [
+                v => !!v || 'Date is required',
+                v => (v && v.length <= 10) || 'Date must be entered in the form DD/MM/YYYY'
+            ],
+            cashRules: [
+                v => !!v || 'Cash Recieved is required',
+                v => (v/1 == v) || 'Enter a number'
+            ],
+            cardRules: [
+                v => !!v || 'Card Recieved is required',
+                v => (v/1 == v) || 'Enter a number'
+            ],
+            dayRules: [
+                v => !!v || 'Day is required',
+            ],
+        }
+    },
+    created(){
+        const sheetID = this.$route.params.sheet_id
+        db.collection('night-sheets').doc(sheetID).get().then(doc => {                         
+                this.sheetName = doc.data().Name
+                this.date = doc.data().Date
+                this.day = doc.data().Day
+                this.notes = doc.data().Notes
+                this.cash = doc.data().Cash
+                this.card = doc.data().Card
+        })
+    },
+    methods:{
+        updateSheet(){
+            const sheetID = this.$route.params.sheet_id
+            db.collection('night-sheets').doc(sheetID).update({
+                Name: this.sheetName,
+                Day: this.day,
+                Date: this.date,
+                Cash: this.cash,
+                Card: this.card,
+                Total: this.cash + this.card,
+                Notes: this.notes
+            })
+            this.$router.push(`/` + sheetID)
         }
     }
 }
