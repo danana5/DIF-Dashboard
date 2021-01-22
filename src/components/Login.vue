32c28a8e7d091e4678ce4a0d41185f95c78b1771
Daniel Grace <daniel.grace44@hotmail.com>
2021-01-22 15:11:57 +0000

    styling changes to login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,24 +1,27 @@
 <template>
   <v-container>
         <v-card
-        class="mx-auto mt-10"
+        class="mx-auto mt-10 blue--text text--darken-3"
         max-width="600"
         :loading="loading"
         :disabled="loading"
         rounded
+        elevation="10"
         >
             <v-container>
                 <form>
-                    <h3>Log In</h3>
+                    <h2 style="text-align: center;">Log In</h2>
                     <v-divider></v-divider>
                     <v-text-field
                     class="mt-5"
                     v-model="email"
                     label="Email"
+                    outlined
                     >
                     </v-text-field>
                     <v-text-field
                     v-model="password"
+                    outlined
                     :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
                     :type="show ? 'text' : 'password'"
                     label="Password"
@@ -26,7 +29,7 @@
                     >
                     </v-text-field>
                     <v-btn
-                    class="white--text" 
+                    class="white--text"
                     color="blue darken-3"
                     outlined
                     rounded
