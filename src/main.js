137dde409fc677ac6018c22fd21537173adaae6e
Daniel Grace <daniel.grace44@hotmail.com>
2021-01-22 01:54:20 +0000

    fixed log out error

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import firebase from "firebase";
+import "./components/firebaseInit";
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  vuetify,
-  render: (h) => h(App),
-}).$mount("#app");
+let app;
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      vuetify,
+      render: (h) => h(App),
+    }).$mount("#app");
+  }
+});
