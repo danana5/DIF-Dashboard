42f5b912e7948b6d0bdfe2b8bbf100dee67321a6
Daniel Grace <daniel.grace44@hotmail.com>
2021-02-22 18:38:35 +0000

    fixed infinite login bug

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,51 +1,21 @@
 <template>
-  <v-container>
-        <v-card
-        class="mx-auto mt-10 blue--text text--darken-3"
-        max-width="600"
-        :loading="loading"
-        :disabled="loading"
-        rounded
-        elevation="10"
-        >
-            <v-container>
-                <form>
-                    <h2 style="text-align: center;">Log In</h2>
-                    <v-divider></v-divider>
-                    <v-text-field
-                    class="mt-5"
-                    v-model="email"
-                    label="Email"
-                    outlined
-                    >
-                    </v-text-field>
-                    <v-text-field
-                    v-model="password"
-                    outlined
-                    :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-                    :type="show ? 'text' : 'password'"
-                    label="Password"
-                    @click:append="show = !show"
-                    ></v-text-field>
-                    <v-checkbox
-                    v-checkbox
-                    v-model="rememberME"
-                    label="Stay Logged in?"
-                    ></v-checkbox>
-                    <v-btn
-                    class="white--text"
-                    color="blue darken-3"
-                    outlined
-                    rounded
-                    text
-                    @click="login"
-                    >
+<v-container>
+    <v-card class="mx-auto mt-10 blue--text text--darken-3" max-width="600" :loading="loading" :disabled="loading" rounded elevation="10">
+        <v-container>
+            <form>
+                <h2 style="text-align: center;">Log In</h2>
+                <v-divider></v-divider>
+                <v-text-field class="mt-5" v-model="email" label="Email" outlined>
+                </v-text-field>
+                <v-text-field v-model="password" outlined :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'" :type="show ? 'text' : 'password'" label="Password" @click:append="show = !show"></v-text-field>
+                <v-checkbox v-checkbox v-model="rememberME" label="Stay Logged in?"></v-checkbox>
+                <v-btn class="white--text" color="blue darken-3" outlined rounded text @click="login">
                     Login
-                    </v-btn>
-                </form>
-            </v-container>
-        </v-card>
-  </v-container>
+                </v-btn>
+            </form>
+        </v-container>
+    </v-card>
+</v-container>
 </template>
 
 <script>
@@ -61,32 +31,33 @@ export default {
             loading: false,
         }
     },
-    methods:{
-        login(){
+    methods: {
+        login() {
             this.loading = true
-            if(this.rememberME){
+            if (this.rememberME) {
                 firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
                 firebase.auth().signInWithEmailAndPassword(this.email, this.password)
-                .then(user => {
-                    console.log("Logged in as " + user.email)
-                    this.$router.push("/dash")
-                    this.loading = false
-                },
-                err => {
-                    alert(err.message)
-                })
-            }
-            else{
+                    .then(user => {
+                            console.log("Logged in as " + user.email)
+                            this.$router.push("/dash")
+                            this.loading = false
+                        },
+                        err => {
+                            alert(err.message)
+                            this.loading = false
+                        })
+            } else {
                 firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
                 firebase.auth().signInWithEmailAndPassword(this.email, this.password)
-                .then(user => {
-                    console.log("Logged in as " + user.email)
-                    this.$router.push("/dash")
-                    this.loading = false
-                },
-                err => {
-                    alert(err.message)
-                })
+                    .then(user => {
+                            console.log("Logged in as " + user.email)
+                            this.$router.push("/dash")
+                            this.loading = false
+                        },
+                        err => {
+                            alert(err.message)
+                            this.loading = false
+                        })
             }
         }
     }
