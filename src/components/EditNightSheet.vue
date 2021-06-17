13f2c4cefecc3d3068c7b3e078b53322928bdcaa
Daniel Grace <daniel.grace44@hotmail.com>
2021-06-17 13:10:47 +0100

    formatting

--- src/components/EditNightSheet.vue
+++ src/components/EditNightSheet.vue
@@ -1,99 +1,48 @@
 <template>
-    <v-container>
-        <v-card
-        class="mx-auto mt-10"
-        max-width="1000"
-        tile
-        :loading="loading"
-        >
-            <v-card-title>
-                {{date}}
-            </v-card-title>
-            <v-card-subtitle>
-                {{day}}
-            </v-card-subtitle>
-            <v-container>
-                <v-form
-                ref="form"
-                v-model="valid"
-                lazy-validation
-                >
-                    <v-text-field
-                    v-model="sheetName"
-                    :rules="nameRules"
-                    label="Name"
-                    placeholder="Name"
-                    required
-                    clearable
-                    >
-                    </v-text-field>
-                    <v-textarea
-                    v-model="notes"
-                    label="Notes"
-                    auto-grow
-                    placeholder="Notes"
-                    clearable
-                    >
-                    </v-textarea>
-                    <v-spacer></v-spacer>
-                    <v-divider></v-divider>
-                    <v-spacer></v-spacer>
-                    <v-row>
-                        <v-col>
-                            <v-text-field
-                            v-model="cash"
-                            :rules="cashRules"
-                            label="Cash Recieved"
-                            required
-                            prepend-icon="€"
-                            clearable
-                            >
-                            </v-text-field>
-                        </v-col>
-                        <v-col>
-                            <v-text-field
-                            v-model="card"
-                            :rules="cardRules"
-                            label="Card Recieved"
-                            required
-                            prepend-icon="€"
-                            clearable
-                            >
-                            </v-text-field>
-                        </v-col>
-                    </v-row>
-                    <v-btn
-                    class="white--text mr-4" 
-                    color="red darken-3"
-                    outlined
-                    rounded
-                    text
-                    :to="`/list`"
-                    >
-                        Cancel
-                    </v-btn>
-                    <v-btn
-                    class="white--text" 
-                    color="blue darken-3"
-                    outlined
-                    :disabled="!valid"
-                    rounded
-                    text
-                    @click="updateSheet"
-                    >
-                        Update Sheet
-                    </v-btn>
-                </v-form>
-            </v-container>
-        </v-card>
-    </v-container>
+<v-container>
+    <v-card class="mx-auto mt-10" max-width="1000" tile :loading="loading">
+        <v-card-title>
+            {{date}}
+        </v-card-title>
+        <v-card-subtitle>
+            {{day}}
+        </v-card-subtitle>
+        <v-container>
+            <v-form ref="form" v-model="valid" lazy-validation>
+                <v-text-field v-model="sheetName" :rules="nameRules" label="Name" placeholder="Name" required clearable>
+                </v-text-field>
+                <v-textarea v-model="notes" label="Notes" auto-grow placeholder="Notes" clearable>
+                </v-textarea>
+                <v-spacer></v-spacer>
+                <v-divider></v-divider>
+                <v-spacer></v-spacer>
+                <v-row>
+                    <v-col>
+                        <v-text-field v-model="cash" :rules="cashRules" label="Cash Recieved" required prepend-icon="€" clearable>
+                        </v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field v-model="card" :rules="cardRules" label="Card Recieved" required prepend-icon="€" clearable>
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+                <v-btn class="white--text mr-4" color="red darken-3" outlined rounded text :to="`/list`">
+                    Cancel
+                </v-btn>
+                <v-btn class="white--text" color="blue darken-3" outlined :disabled="!valid" rounded text @click="updateSheet">
+                    Update Sheet
+                </v-btn>
+            </v-form>
+        </v-container>
+    </v-card>
+</v-container>
 </template>
 
 <script>
 import db from './firebaseInit'
 export default {
     name: 'EditNightSheet',
-    data () {
+    data() {
         return {
             valid: true,
             date: null,
@@ -112,34 +61,34 @@ export default {
             ],
             cashRules: [
                 v => !!v || 'Cash Recieved is required',
-                v => (v/1 == v) || 'Enter a number'
+                v => (v / 1 == v) || 'Enter a number'
             ],
             cardRules: [
                 v => !!v || 'Card Recieved is required',
-                v => (v/1 == v) || 'Enter a number'
+                v => (v / 1 == v) || 'Enter a number'
             ],
             dayRules: [
                 v => !!v || 'Day is required',
             ],
-            loading:false,
+            loading: false,
         }
     },
-    created(){
+    created() {
         this.loading = true
         const sheetID = this.$route.params.sheet_id
-        db.collection('night-sheets').doc(sheetID).get().then(doc => {                         
-                this.sheetName = doc.data().Name
-                this.date = doc.data().Date
-                this.day = doc.data().Day
-                this.notes = doc.data().Notes
-                this.cash = doc.data().Cash
-                this.card = doc.data().Card
-                this.loading = false
+        db.collection('night-sheets').doc(sheetID).get().then(doc => {
+            this.sheetName = doc.data().Name
+            this.date = doc.data().Date
+            this.day = doc.data().Day
+            this.notes = doc.data().Notes
+            this.cash = doc.data().Cash
+            this.card = doc.data().Card
+            this.loading = false
         })
     },
-    methods:{
-        updateSheet(){
-            this.loading=true
+    methods: {
+        updateSheet() {
+            this.loading = true
             const sheetID = this.$route.params.sheet_id
             db.collection('night-sheets').doc(sheetID).update({
                 Name: this.sheetName,
