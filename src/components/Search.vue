a7aab87abfc9227aa5d4bc518bac053d47452d95
Daniel Grace <daniel.grace44@hotmail.com>
2023-01-18 19:07:22 +0000

    Formatting changes

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -2,45 +2,22 @@
   <v-container>
     <div class="searchBar">
       <v-row>
-        <v-layout
-          align-content-center
-          style="max-width: 30%; margin: 0 auto; min-width: 300px"
-        >
-          <v-menu
-            ref="menu"
-            v-model="menu"
-            :close-on-content-click="false"
-            :return-value.sync="query"
-            transition="scale-transition"
-            offset-y
-            min-width="auto"
-          >
+        <v-layout align-content-center style="max-width: 30%; margin: 0 auto; min-width: 300px">
+          <v-menu ref="menu" v-model="menu" :close-on-content-click="false" :return-value.sync="query"
+            transition="scale-transition" offset-y min-width="auto">
             <template v-slot:activator="{ on, attrs }">
-              <v-text-field
-                class="mt-10"
-                v-model="query"
-                label="Choose a date"
-                append-outer-icon="mdi-magnify"
-                readonly
-                @click:append-outer="submit"
-                @keyup.enter="submit"
-                v-bind="attrs"
-                v-on="on"
-              ></v-text-field>
+              <v-text-field class="mt-10" v-model="query" label="Choose a date" append-outer-icon="mdi-magnify" readonly
+                @click:append-outer="submit" @keyup.enter="submit" v-bind="attrs" v-on="on"></v-text-field>
             </template>
             <v-date-picker v-model="query" no-title scrollable>
               <v-spacer></v-spacer>
               <v-btn text color="primary" @click="menu = false">
                 Cancel
               </v-btn>
-              <v-btn
-                text
-                color="primary"
-                @click="
-                  $refs.menu.save(query);
-                  submit();
-                "
-              >
+              <v-btn text color="primary" @click="
+                $refs.menu.save(query);
+              submit();
+              ">
                 OK
               </v-btn>
             </v-date-picker>
@@ -67,27 +44,13 @@
         <strong>Total Recieved:</strong> €{{ resultSheet.total }}
       </v-card-text>
       <v-card-actions>
-        <v-btn
-          outlined
-          rounded
-          text
-          class="white--text"
-          color="blue darken-3"
-          :to="`edit/${resultSheet.id}`"
-        >
+        <v-btn outlined rounded text class="white--text" color="blue darken-3" :to="`edit/${resultSheet.id}`">
           <v-icon left>
             mdi-pencil
           </v-icon>
           Edit
         </v-btn>
-        <v-btn
-          outlined
-          rounded
-          text
-          class="white--text"
-          color="red darken-3"
-          @click.stop="dialog = true"
-        >
+        <v-btn outlined rounded text class="white--text" color="red darken-3" @click.stop="dialog = true">
           <v-icon left>
             mdi-file-remove-outline
           </v-icon>
@@ -101,54 +64,54 @@
   </v-container>
 </template>
 <script>
-  import db from './firebaseInit';
-  export default {
-    name: 'search',
-    data() {
-      return {
-        query: null,
-        nightSheets: new Map(),
-        resultSheet: null,
-        found: false,
-        menu: false,
-        failed: false,
-        searchedQuery: null,
-      };
-    },
-    created() {
-      db.collection('night-sheets')
-        .orderBy('Date')
-        .get()
-        .then((querySnapshot) => {
-          querySnapshot.forEach((doc) => {
-            const data = {
-              id: doc.id,
-              name: doc.data().Name,
-              date: doc.data().Date,
-              day: doc.data().Day,
-              notes: doc.data().Notes,
-              cash: doc.data().Cash,
-              card: doc.data().Card,
-              total: doc.data().Total,
-            };
-            this.nightSheets.set(doc.data().Date, data);
-          });
+import db from './firebaseInit';
+export default {
+  name: 'search',
+  data() {
+    return {
+      query: null,
+      nightSheets: new Map(),
+      resultSheet: null,
+      found: false,
+      menu: false,
+      failed: false,
+      searchedQuery: null,
+    };
+  },
+  created() {
+    db.collection('night-sheets')
+      .orderBy('Date')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          const data = {
+            id: doc.id,
+            name: doc.data().Name,
+            date: doc.data().Date,
+            day: doc.data().Day,
+            notes: doc.data().Notes,
+            cash: doc.data().Cash,
+            card: doc.data().Card,
+            total: doc.data().Total,
+          };
+          this.nightSheets.set(doc.data().Date, data);
         });
+      });
+  },
+  methods: {
+    submit() {
+      console.log(this.nightSheets);
+      this.found = false;
+      this.searchedQuery = this.query;
+      this.failed = false;
+      this.resultSheet = null;
+      if (this.nightSheets.has(this.searchedQuery)) {
+        this.found = true;
+        this.resultSheet = this.nightSheets.get(this.searchedQuery);
+      } else {
+        this.failed = true;
+      }
     },
-    methods: {
-      submit() {
-        console.log(this.nightSheets);
-        this.found = false;
-        this.searchedQuery = this.query;
-        this.failed = false;
-        this.resultSheet = null;
-        if (this.nightSheets.has(this.searchedQuery)) {
-          this.found = true;
-          this.resultSheet = this.nightSheets.get(this.searchedQuery);
-        } else {
-          this.failed = true;
-        }
-      },
-    },
-  };
+  },
+};
 </script>
